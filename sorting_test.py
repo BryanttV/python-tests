7916cfe1a342be8ef001d8a4e765c7a1a8b7e69c
BryanttV <bryanttandrei2015@gmail.com>
2022-10-13 02:11:11 +0000

    feat: improved sort function

--- sorting_test.py
+++ sorting_test.py
@@ -12,37 +12,16 @@ FIELDS_LIST = [
 ]
 
 
-def update_position_db(field_uuid: str, new_pos: int) -> int:
+def update_position_db(
+    field_uuid: str, new_pos: int, fields_list: list[dict[str, Any]]
+) -> int:
     """Updates position in db"""
-    for field in FIELDS_LIST:
+    for field in fields_list:
         if field.get("uuid") == field_uuid:
             current_pos = field.get("sort")
-            field[field_uuid] = new_pos
+            field["sort"] = new_pos
 
     return current_pos
-
-
-def update_position(
-    field_uuid: str, new_pos: int, fields_list: list[dict[str, Any]]
-) -> Union[dict, str]:
-    """Update the position of a field"""
-    update_position_db(field_uuid, new_pos)
-    fields_dict: dict[str, dict] = {field["uuid"]: field for field in fields_list}
-    current_pos: int = fields_dict[field_uuid]["sort"]
-
-    if new_pos > len(fields_list) or new_pos < 1 or new_pos == current_pos:
-        return "Invalid number"
-
-    fields_dict[field_uuid]["sort"] = new_pos
-
-    if current_pos < new_pos:
-        for value in list(fields_dict.values())[current_pos:new_pos]:
-            value["sort"] -= 1
-    elif new_pos < current_pos:
-        for value in list(fields_dict.values())[new_pos - 1 : current_pos - 1]:
-            value["sort"] += 1
-
-    return dict(sorted(fields_dict.items(), key=lambda x: x[1]["sort"]))
 
 
 def sort_list(fields_list: list[dict[str, Any]]) -> list[dict[str, Any]]:
@@ -50,5 +29,25 @@ def sort_list(fields_list: list[dict[str, Any]]) -> list[dict[str, Any]]:
     return sorted(fields_list, key=lambda x: x["sort"])
 
 
+def update_position(
+    field_uuid: str, new_pos: int, fields_list: list[dict[str, Any]]
+) -> Union[dict, str]:
+    """Update the position of a field"""
+    current_pos = update_position_db(field_uuid, new_pos, fields_list)
+
+    if new_pos > len(fields_list) or new_pos < 1 or new_pos == current_pos:
+        return "Invalid number"
+
+    if current_pos < new_pos:
+        for field in fields_list[current_pos:new_pos]:
+            field["sort"] -= 1
+    elif new_pos < current_pos:
+        for field in fields_list[new_pos - 1 : current_pos - 1]:
+            field["sort"] += 1
+
+    return sort_list(fields_list)
+
+
 pprint(update_position("uuid5", 1, sort_list(FIELDS_LIST)), sort_dicts=False)
 pprint(update_position("uuid1", 5, sort_list(FIELDS_LIST)), sort_dicts=False)
+pprint(update_position("uuid3", 2, sort_list(FIELDS_LIST)), sort_dicts=False)
